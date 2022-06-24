67d61a0222bf728ca242f23f7ce28aa66a341331
Zac-Mwangi <zackmwangi998@gmail.com>
2022-06-24 15:56:20 +0300

    json from gh-pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,14 +3,45 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   //   console.log("DOM fully loaded and parsed");
 
-  getProduct_type();
+  loadData();
 
-  getProduct("6kg");
+  
+
+  //  getProduct_type();
+
+  // getProduct("6kg");
 });
 
 // const url = "http://localhost:3000/";
-const url = "https://zac-mwangi.github.io/LPG-Online-Shop/db.json/";
+let datadd;
+let productsData;
+const url = "https://zac-mwangi.github.io/LPG-Online-Shop/assets/db/db.json";
 let amount = 0;
+
+// load every data
+function loadData() {
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      datadd = data;
+      productsData = datadd.product;
+      console.log(datadd.product.length);
+
+      getProduct_type(
+        datadd.product_type,
+        datadd.product_type.length,
+        datadd.product.length,
+        productsData
+      );
+
+      getProduct(data[i].type, productLength, productsData);
+
+
+      // getProduct("6kg");
+    });
+}
 
 let cartAmount = document.createElement("button");
 let links = document.getElementById("links");
@@ -23,120 +54,106 @@ cartAmount.addEventListener("click", () => {
   links.appendChild(cartAmount);
 });
 
-function getProduct_type() {
-  fetch(url + "product_type/")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      for (let i = 0; i < data.length; i++) {
-        let li = document.createElement("li");
-        li.innerText = data[i].type;
-        links.appendChild(li);
+function getProduct_type(data, length, productLength, productsData) {
+  for (let i = 0; i < length; i++) {
+    // console.log(data[1])
+    let li = document.createElement("li");
+    li.innerText = data[i].type;
+    links.appendChild(li);
 
-        li.addEventListener("click", () => {
-          // heading
+    li.addEventListener("click", () => {
+      let head = document.getElementById("product_heading");
+      head.innerText = data[i].type + " Available";
 
-          //   li.style.color = "#EC4067"
-          //   li.style.fontSize = "25px"
-
-          let head = document.getElementById("product_heading");
-          head.innerText = data[i].type + " Available";
-          getProduct(data[i].type);
-        });
-
-        // list
-        li.addEventListener("mouseover", () => {
-          // h.style.color = "#EDD927";
-          li.style.color = "#EC4067";
-          li.style.fontSize = "25px";
-        });
-        li.addEventListener("mouseout", () => {
-          // h.style.color = "white";
-          li.style.color = "#fff";
-          li.style.fontSize = "20px";
-        });
-      }
-      let cartLi = document.createElement("li");
-      cartAmount.innerText = "Cart : " + amount;
-      cartLi.appendChild(cartAmount);
-      links.appendChild(cartLi);
+      //  console.log(productsData)
+      getProduct(data[i].type, productLength, productsData);
     });
+
+    // list
+    li.addEventListener("mouseover", () => {
+      // h.style.color = "#EDD927";
+      li.style.color = "#EC4067";
+      li.style.fontSize = "25px";
+    });
+    li.addEventListener("mouseout", () => {
+      // h.style.color = "white";
+      li.style.color = "#fff";
+      li.style.fontSize = "20px";
+    });
+  }
+  let cartLi = document.createElement("li");
+  cartAmount.innerText = "Cart : " + amount;
+  cartLi.appendChild(cartAmount);
+  links.appendChild(cartLi);
 }
 
-function getProduct(type) {
-  fetch(url + "product/")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      let content = document.getElementById("content_id");
+function getProduct(type, productLength, dt) {
+  console.log(type + " length : " + productLength + " data : " + productsData);
+  let content = document.getElementById("content_id");
 
-      content.innerHTML = "";
-      for (let i = 0; i < data.length; i++) {
-        let prd_type = data[i].product_type;
-        let prd_name = data[i].product_name;
-        if (prd_type.toLowerCase() === type.toLowerCase()) {
-          let productDiv = document.createElement("div");
-          productDiv.className = "product";
+  for (let i = 0; i < productLength; i++) {
+    let prd_type = productsData[i].product_type;
+    let prd_name = productsData[i].product_name;
+    if (prd_type.toLowerCase() === type.toLowerCase()) {
+      console.log(productsData[i]);
 
-          let img_divvv = document.createElement("div");
-          img_divvv.className = "img_divvv";
+      let productDiv = document.createElement("div");
+      productDiv.className = "product";
 
-          let img = document.createElement("img");
-          img.src = data[i].image;
-          //   img_divvv.appendChild(img)
+      let img_divvv = document.createElement("div");
+      img_divvv.className = "img_divvv";
 
-          let p1 = document.createElement("p");
-          p1.innerText = data[i].product_name;
+      let img = document.createElement("img");
+      img.src = productsData[i].image;
+      //   img_divvv.appendChild(img)
 
-          let p2 = document.createElement("p");
-          p2.innerText = data[i].product_name;
+      let p1 = document.createElement("p");
+      p1.innerText = productsData[i].product_name;
 
-          let p3 = document.createElement("p");
-          p3.innerText = "Ksh. " + data[i].retail_price;
+      let p2 = document.createElement("p");
+      p2.innerText = productsData[i].product_name;
 
-          let btn = document.createElement("button");
-          btn.innerText = "Add to Cart   ";
-          btn.className = "add_to_cart";
-          btn.id = "add_to_cart";
+      let p3 = document.createElement("p");
+      p3.innerText = "Ksh. " + productsData[i].retail_price;
 
-          let br = document.createElement("br");
-          let br2 = document.createElement("br");
+      let btn = document.createElement("button");
+      btn.innerText = "Add to Cart   ";
+      btn.className = "add_to_cart";
+      btn.id = "add_to_cart";
 
-          let img_cart = document.createElement("img");
-          img_cart.src =
-            "https://cdn-icons-png.flaticon.com/512/3737/3737151.png";
+      let br = document.createElement("br");
+      let br2 = document.createElement("br");
 
-          btn.appendChild(img_cart);
+      let img_cart = document.createElement("img");
+      img_cart.src = "https://cdn-icons-png.flaticon.com/512/3737/3737151.png";
 
-          productDiv.appendChild(img);
-          productDiv.appendChild(br);
-          productDiv.appendChild(p1);
-          productDiv.appendChild(p2);
-          productDiv.appendChild(p3);
-          productDiv.appendChild(br2);
-          productDiv.appendChild(btn);
+      btn.appendChild(img_cart);
 
-          content.appendChild(productDiv);
+      productDiv.appendChild(img);
+      productDiv.appendChild(br);
+      productDiv.appendChild(p1);
+      productDiv.appendChild(p2);
+      productDiv.appendChild(p3);
+      productDiv.appendChild(br2);
+      productDiv.appendChild(btn);
 
-          btn.addEventListener("click", () => {
-            let btnValue = btn.innerHTML;
+      content.appendChild(productDiv);
 
-            amount = parseInt(amount) + parseInt(data[i].retail_price);
+      btn.addEventListener("click", () => {
+        let btnValue = btn.innerHTML;
 
-            cartAmount.innerText = "Cart : " + amount;
-            links.appendChild(cartAmount);
-          });
+        amount = parseInt(amount) + parseInt(productsData[i].retail_price);
 
-          btn.addEventListener("mouseover", () => {
-            btn.style.color = "#000";
-          });
-          btn.addEventListener("mouseout", () => {
-            btn.style.color = "white";
-          });
-        }
-      }
-      //   console.log(arr)
-    });
+        cartAmount.innerText = "Cart : " + amount;
+        links.appendChild(cartAmount);
+      });
+
+      btn.addEventListener("mouseover", () => {
+        btn.style.color = "#000";
+      });
+      btn.addEventListener("mouseout", () => {
+        btn.style.color = "white";
+      });
+    }
+  }
 }
---
+++ assets/js/index2.js
@@ -0,0 +1,140 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  //   console.log("DOM fully loaded and parsed");
+
+  getProduct_type();
+
+  getProduct("6kg");
+});
+
+const url = "http://localhost:3000/";
+// const url = "https://zac-mwangi.github.io/LPG-Online-Shop/db.json/";
+let amount = 0;
+
+let cartAmount = document.createElement("button");
+let links = document.getElementById("links");
+
+cartAmount.addEventListener("click", () => {
+  // heading
+  alert("You are about to clear the Cart ");
+  amount = 0;
+  cartAmount.innerText = "Cart : " + amount;
+  links.appendChild(cartAmount);
+});
+
+function getProduct_type() {
+  fetch(url + "product_type/")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (let i = 0; i < data.length; i++) {
+        let li = document.createElement("li");
+        li.innerText = data[i].type;
+        links.appendChild(li);
+
+        li.addEventListener("click", () => {
+          // heading
+
+          //   li.style.color = "#EC4067"
+          //   li.style.fontSize = "25px"
+
+          let head = document.getElementById("product_heading");
+          head.innerText = data[i].type + " Available";
+          getProduct(data[i].type);
+        });
+
+        // list
+        li.addEventListener("mouseover", () => {
+          // h.style.color = "#EDD927";
+          li.style.color = "#EC4067";
+          li.style.fontSize = "25px";
+        });
+        li.addEventListener("mouseout", () => {
+          // h.style.color = "white";
+          li.style.color = "#fff";
+          li.style.fontSize = "20px";
+        });
+      }
+      let cartLi = document.createElement("li");
+      cartAmount.innerText = "Cart : " + amount;
+      cartLi.appendChild(cartAmount);
+      links.appendChild(cartLi);
+    });
+}
+
+function getProduct(type) {
+  fetch(url + "product/")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let content = document.getElementById("content_id");
+
+      content.innerHTML = "";
+      for (let i = 0; i < data.length; i++) {
+        let prd_type = data[i].product_type;
+        let prd_name = data[i].product_name;
+        if (prd_type.toLowerCase() === type.toLowerCase()) {
+          let productDiv = document.createElement("div");
+          productDiv.className = "product";
+
+          let img_divvv = document.createElement("div");
+          img_divvv.className = "img_divvv";
+
+          let img = document.createElement("img");
+          img.src = data[i].image;
+          //   img_divvv.appendChild(img)
+
+          let p1 = document.createElement("p");
+          p1.innerText = data[i].product_name;
+
+          let p2 = document.createElement("p");
+          p2.innerText = data[i].product_name;
+
+          let p3 = document.createElement("p");
+          p3.innerText = "Ksh. " + data[i].retail_price;
+
+          let btn = document.createElement("button");
+          btn.innerText = "Add to Cart   ";
+          btn.className = "add_to_cart";
+          btn.id = "add_to_cart";
+
+          let br = document.createElement("br");
+          let br2 = document.createElement("br");
+
+          let img_cart = document.createElement("img");
+          img_cart.src =
+            "https://cdn-icons-png.flaticon.com/512/3737/3737151.png";
+
+          btn.appendChild(img_cart);
+
+          productDiv.appendChild(img);
+          productDiv.appendChild(br);
+          productDiv.appendChild(p1);
+          productDiv.appendChild(p2);
+          productDiv.appendChild(p3);
+          productDiv.appendChild(br2);
+          productDiv.appendChild(btn);
+
+          content.appendChild(productDiv);
+
+          btn.addEventListener("click", () => {
+            let btnValue = btn.innerHTML;
+
+            amount = parseInt(amount) + parseInt(data[i].retail_price);
+
+            cartAmount.innerText = "Cart : " + amount;
+            links.appendChild(cartAmount);
+          });
+
+          btn.addEventListener("mouseover", () => {
+            btn.style.color = "#000";
+          });
+          btn.addEventListener("mouseout", () => {
+            btn.style.color = "white";
+          });
+        }
+      }
+      //   console.log(arr)
+    });
+}
